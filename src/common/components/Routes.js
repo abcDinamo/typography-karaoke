20d556e8b6ac35a07dcb9765ed17d6f5a819b5e0
mhgbrown <brown.mhg+git@gmail.com>
2016-08-26 21:24:01 +0200

    Adapt routes for github deploy

--- src/common/components/Routes.js
+++ src/common/components/Routes.js
@@ -5,8 +5,14 @@ import App from './App';
 import HomePage from '../../pages/home/page';
 import KaraokePage from '../../pages/karaoke/page';
 
+var path = "/";
+
+if(window.location.href.indexOf('github') >= 0) {
+  path = path + 'typography-karaoke/';
+}
+
 export default (
-  <Route path="/" component={App}>
+  <Route path={ path } component={App}>
     <IndexRoute component={HomePage} />
     <Route path="karaoke/:id" component={KaraokePage} />
   </Route>
