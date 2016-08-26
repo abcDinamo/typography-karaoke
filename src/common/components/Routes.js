3133cdae0e38298d5e8b72fc37676c0dff53914b
mhgbrown <brown.mhg+git@gmail.com>
2016-08-26 19:41:35 +0200

    Add route to karaoke page

--- src/common/components/Routes.js
+++ src/common/components/Routes.js
@@ -3,10 +3,11 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './App';
 import HomePage from '../../pages/home/page';
-
+import KaraokePage from '../../pages/karaoke/page';
 
 export default (
-  <Route path="*" component={App}>
+  <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
+    <Route path="karaoke/:id" component={KaraokePage} />
   </Route>
 );
---
+++ src/pages/karaoke/page.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import trackStore from '../../common/store/TrackStore.js'
+
+import styles from "./style.css";
+
+export default class KaraokePage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul>
+
+      </ul>
+    );
+  }
+}
