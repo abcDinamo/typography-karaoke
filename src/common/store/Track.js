93f36e67174fbd118ee8a75563b22ff37ecbd4b5
mhgbrown <brown.mhg+git@gmail.com>
2016-08-26 19:41:17 +0200

    Create Track and TrackStore

---
+++ src/common/store/Track.js
@@ -0,0 +1,8 @@
+export default class Track {
+  constructor(id, name, recording, subtitles) {
+    this.id = id;
+    this.name = name;
+    this.recording = recording;
+    this.subtitles = subtitles;
+  }
+}
---
+++ src/common/store/TrackStore.js
@@ -0,0 +1,34 @@
+import _ from 'lodash';
+
+import Track from './Track';
+
+import alphabetTrack from '../../assets/music/01-Alphabet-Song/track.mp4';
+import alphabetSubtitles from '../../assets/music/01-Alphabet-Song/subtitles.vtt';
+
+import frereTrack from '../../assets/music/02-Frere-Jacques/track.mp4';
+import FrereSubtitles from '../../assets/music/02-Frere-Jacques/subtitles.vtt';
+
+import happyTrack from '../../assets/music/03-If-you-are-Happy/track.mp4';
+import happySubtitles from '../../assets/music/03-If-you-are-Happy/subtitles.vtt';
+
+class TrackStore {
+  constructor() {
+    this.tracks = [];
+  }
+
+  addTrack(track) {
+    this.tracks.push(track);
+  }
+
+  getTrackById(id) {
+    // FIXME for now this works because it's just 3 things
+    return _.find(this.tracks, ['id', id]);
+  }
+}
+
+var trackStore = new TrackStore();
+trackStore.addTrack(new Track('Alphabet Song', 'Alphabet Song', alphabetTrack, alphabetSubtitles));
+trackStore.addTrack(new Track('Frere Jacques', 'Frere Jacques', alphabetTrack, alphabetSubtitles));
+trackStore.addTrack(new Track('If you are Happy', 'If you are Happy', alphabetTrack, alphabetSubtitles));
+
+export default trackStore;
