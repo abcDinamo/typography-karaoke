700e3b59f7d1b5edec571692ad0912fac6412b40
mhgbrown <brown.mhg+git@gmail.com>
2016-09-12 20:50:14 +0200

    Add Contentful base

---
+++ src/common/store/Contentful.js
@@ -0,0 +1,8 @@
+import Contentful from 'contentful';
+
+var client = Contentful.createClient({
+  space: '2z9zgicdw8i6',
+  accessToken: '056d805fdd5a339f95b444f0affeb77d37277a468bbf5ea6452b53f7e679f879'
+});
+
+export default client;
