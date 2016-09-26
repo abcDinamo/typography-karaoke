ad8d87d3e590a9611bbf831013b25ee0cab5fadb
mhgbrown <brown.mhg+git@gmail.com>
2016-09-27 00:30:17 +0200

    Make sure react minification is happening

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -13,7 +13,12 @@ module.exports = {
   },
 
   plugins: [
-    new ExtractTextPlugin('style.css', { allChunks: true })
+    new ExtractTextPlugin('style.css', { allChunks: true }),
+    new webpack.DefinePlugin({
+      "process.env": {
+         NODE_ENV: JSON.stringify("production")
+       }
+    })
   ],
 
   module: {
